04c010a63fb6fa202d56d548bd3726ae9a28e69e
vinaym97 <vinaymadamana4u@gmail.com>
2021-07-24 13:29:13 +0530

    second commit

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,1 +1,9 @@
-# Write your code here
+import random
+list = ['python','java','kotlin','javascript']
+print("H A N G M A N")
+x = input("Guess the word >")
+if x == random.choice(list):
+    print("You survived!")
+else:
+    print("You lost!")
+
