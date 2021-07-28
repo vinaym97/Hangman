cd1bed9aa26098d9ece0248e1a4f8fad3e803673
vinaym97 <vinaymadamana4u@gmail.com>
2021-07-28 10:47:08 +0530

    corrected

--- Topics/Search in a string/Checking email/main.py
+++ Topics/Search in a string/Checking email/main.py
@@ -1,39 +1,30 @@
-# def check_email(string):
-#     if (" " not in string) and ('@' in string):
-#         # if '@' in string:
-#         if '@.' not in string:
-#             ind = string.find('@')
-#             Print("True")
-#             if "." in string[ind:]:
-#                 return True
-#         else:
-#             return False
-#         # else:
-#         #         return False
-#     else:
-#         return False
-# x = input()
-# print(check_email(x))
+#
 # import re
-# def check_email(address):
-#   # Checks if the address match regular expression
-#   is_valid = re.search('^\w.+@\w+.\w+$', address)
-#   # If there is a matching group
-#   if is_valid:
-#     return True
-#   else:
+#
+# regex = r'\b[A-Za-z0-9._%+-]+email@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+#
+# def check_email(email):
+#   if(re.match(regex, email)):
+#     # print("True")
+#     # return True
+#   # else:
+#     print("False")
 #     return False
-import re
-
-regex = r'\b[A-Za-z0-9._%+-]+email@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-
-def check(email):
-  if(re.match(regex, email)):
-    print("True")
-
-  else:
-    print("False")
-
 # Driver Code
-x = input()
-check(x)
+# x = input()
+# check(x)
+def check_email(email):
+  index = email.find("@")
+
+  if " " in email:
+    return False
+  elif "@" not in email:
+    return False
+  elif "." not in email:
+    return False
+  elif "@." in email:
+    return False
+  elif "." not in email[index:]:
+    return False
+  else:
+    return True
