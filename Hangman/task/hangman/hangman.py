0ef0961b02d4186a6b1da62419ce2573ffb56f6d
Vinay M <69107994+vinaym97@users.noreply.github.com>
2021-08-17 19:08:57 +0530

    updated

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,24 +1,60 @@
 import random
+
+pro = ""
 list = ['python', 'java', 'kotlin', 'javascript']
 print("H A N G M A N\n")
 guess = random.choice(list)
 product = []
 newstring = ""
+repeat = set()
 for i in guess:
     product.append("-")
 count = 0
-while count < 8:
+while True:
+    if count == 8:
+        print("You lost!")
+        break
+    print()
+    if pro.join(product).replace(" ", "") == guess:
+        print()
+        print(pro.join(product).replace(" ", ""))
+        print("You guessed the word!")
+        print("You survived!")
+        break
+        if count == 8:
+            break
     print(newstring.join(product))
     x = input("Input a letter:")
-    if product != guess:
+    if len(x) != 1:
+        print("You should input a single letter")
+        continue
+    if x.islower() == False or x.isalpha() == False:
+        print("Please enter a lowercase English letter")
+        continue
+    if x in repeat:
+        print("You've already guessed this letter")
+        continue
+    if pro.join(product).replace(" ", "") != guess:
         if x in guess:
-            for i in range(0, len(guess)):
-                if x == guess[i]:
-                    product[i] = x
+            if x in set(product):
+                print("No improvements")
+                if count == 8:
+                    break
+            else:
+                count = count - 1
+                for i in range(0, len(guess)):
+                    if x == guess[i]:
+                        #                         if x not in product:
+                        product[i] = x
+                        if count == 8:
+                            break
         else:
             print("That letter doesn't appear in the word")
-    print()
+    if count == 8:
+        print("You lost!")
+        break
     count = count + 1
-else:
-    print("Thanks for playing!\nWe'll see how well you did in the next stage")
 
+    repeat.add(x)
+else:
+    print("You lost!")
